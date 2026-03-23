5a82777867b466d1f2f03c5d353e0c8656156755
fyziktom <tomas.svoboda@technicinsider.com>
2026-03-23 10:15:02 -0400

    improvements

--- src/CanDoItAll.Modules.Factory/Pages/PromptFactoryPage.CanvasInspector.cs
+++ src/CanDoItAll.Modules.Factory/Pages/PromptFactoryPage.CanvasInspector.cs
@@ -8,9 +8,16 @@ public partial class PromptFactoryPage
     private ElementReference floatingInspectorPanel;
     private ElementReference floatingInspectorHandle;
     private bool isCanvasInspectorMinimized;
+    private bool showComponentEditorModal;
 
     private void ToggleCanvasInspector()
         => isCanvasInspectorMinimized = !isCanvasInspectorMinimized;
+
+    private void OpenComponentEditorModal()
+        => showComponentEditorModal = true;
+
+    private void CloseComponentEditorModal()
+        => showComponentEditorModal = false;
 
     private async Task DockCanvasInspectorAsync()
     {
