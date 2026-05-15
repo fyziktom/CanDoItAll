d2df75c1508c960e5b0922f84db3d45f8cdd9126
fyziktom <tomas.svoboda@technicinsider.com>
2026-05-14 20:11:26 -0400

    added timestepper component

--- src/CanDoItAll.Components.BaseLib/Components/DataDisplay/FoundationModels.cs
+++ src/CanDoItAll.Components.BaseLib/Components/DataDisplay/FoundationModels.cs
@@ -15,6 +15,25 @@ public sealed record TimelineItem
     public string Tone { get; init; } = "neutral";
 }
 
+public enum TimelineStepperMode
+{
+    Automatic = 0,
+    Manual = 1
+}
+
+public sealed record TimelineStepperTick
+{
+    public int Step { get; init; }
+
+    public required string Label { get; init; }
+
+    public string? Description { get; init; }
+
+    public bool Major { get; init; }
+
+    public bool Disabled { get; init; }
+}
+
 public sealed record EntityPickerItem
 {
     public required string Id { get; init; }
